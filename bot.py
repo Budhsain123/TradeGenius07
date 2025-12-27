523e89d3574daab24ac65c1a608b9904ca363e6c
TradeGenius07 <149088005+Budhsain123@users.noreply.github.com>
2025-12-27 17:12:15 +0530

    Add files via upload

--- bot.py
+++ bot.py
@@ -33,7 +33,7 @@ load_dotenv()
 # --- Configuration ---
 API_ID = int(os.environ.get("API_ID"))
 API_HASH = os.environ.get("API_HASH")
-BOT_TOKEN = os.environ.get("8271744231:AAFkzZGHp_hUCI_50eECr5RdC_78oJMcOQs")
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
 MONGO_URI = os.environ.get("MONGO_URI")
 LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL")) 
 UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL") 
